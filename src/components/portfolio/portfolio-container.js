e6287521e1bea41796a832d4adf4c71196d77995
bapplewh <bapps.5@gmail.com>
2021-03-07 19:04:44 -0700

    second commit

---
+++ src/components/portfolio/portfolio-container.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+import PortfolioItem from './portfolio-item';
+
+export default class PortfolioContainer extends Component {
+    constructor() {
+        super();
+
+        console.log("Portfolio container has rendered");
+    }
+
+    portfolioItems () {
+        const data = ["Quip", "Eventbrite", "Ministry Safe", "Swingaway"];
+
+        return data.map(item => {
+            return <PortfolioItem title={item} />;
+        })
+    }
+    
+    render() {
+        return (
+            <div>
+                <h2>Portfolio items go here...</h2>
+
+                {this.portfolioItems()}
+            </div>
+        );
+    }
+}

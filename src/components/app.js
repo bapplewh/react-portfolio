22e1a96766da10f7043f9bfa2bb7e2abbe7d5c29
bapplewh <bapps.5@gmail.com>
2021-03-21 23:00:32 -0600

    finalizing authorization component, login and logout features

--- src/components/app.js
+++ src/components/app.js
@@ -4,6 +4,7 @@ import {
   Switch,
   Route
 } from 'react-router-dom';
+import axios from 'axios';
 
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
@@ -15,19 +16,103 @@ import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loggedInStatus: "NOT_LOGGED_IN"
+    }
+
+    this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind(this);
+    this.handleUnSuccessfulLogin = this.handleUnSuccessfulLogin.bind(this);
+    this.handleSuccessfulLogout = this.handleSuccessfulLogout.bind(this);
+  }
+
+  handleSuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "LOGGED_IN"
+    });
+  }
+
+  handleUnSuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN"
+    });
+  }
+
+  handleSuccessfulLogout() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN"
+    });
+  }
+
+  checkLoginStatus() {
+    return axios
+      .get("https://api.devcamp.space/logged_in", { 
+        withCredentials: true 
+      })
+      .then(response => {
+        const loggedIn = response.data.logged_in;
+        const loggedInStatus = this.state.loggedInStatus;
+
+        // 1st condition: checks if we're loggedIn and the status is LOGGED_IN => return data
+        // 2nd condition: checks if we're loggedIn and the status is NOT_LOGGED_IN => update state
+        // 3rd condition: if we're not loggedIn and the status is LOGGED_IN => update state
+
+        if (loggedIn && loggedInStatus === "LOGGED_IN") {
+          return loggedIn;
+        } else if (loggedIn && loggedInStatus === "NOT_LOGGED_IN") {
+          this.setState({
+            loggedInStatus: "LOGGED_IN"
+          });
+        } else if (!loggedIn && loggedInStatus === "LOGGED_IN") {
+          this.setState({
+            loggedInStatus: "NOT_LOGGED_IN"
+          });
+        }
+      })
+      .catch(error => {
+        console.log("Error", error);
+      });
+  }
+
+  componentDidMount() {
+    this.checkLoginStatus();
+  }
+
+  authorizedPages() {
+    return [<Route path="/blog" component={Blog} />]
+  }
+
   render() {
     return (
       <div className='container'>
         <Router>
           <div>
-            <NavigationContainer />
+            <NavigationContainer 
+              loggedInStatus={this.state.loggedInStatus}
+              handleSuccessfulLogout={this.handleSuccessfulLogout}  
+            />
+
+            <h2>{this.state.loggedInStatus}</h2>
 
             <Switch>
               <Route exact path="/" component={Home} />
-              <Route path="/auth" component={Auth} />
+
+              <Route 
+                path="/auth" 
+                render={props => (
+                  <Auth 
+                    {...props}
+                    handleSuccessfulLogin={this.handleSuccessfulLogin}
+                    handleUnSuccessfulLogin={this.handleUnSuccessfulLogin}
+                  />
+                )}
+              />
+
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
-              <Route path="/blog" component={Blog} />
+              {this.state.loggedInStatus === "LOGGED_IN" ? this.authorizedPages() : null}
               <Route exact path="/portfolio/:slug" component={PortfolioDetail} />
               <Route component={NoMatch} />
             </Switch>
--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -36,16 +36,18 @@ export default class Login extends Component {
             )
             .then(response => {
                 if (response.data.status === 'created') {
-                    console.log("You can come in...")
+                    this.props.handleSuccessfulAuth();
                 } else {
                     this.setState({
                         errorText: "Wrong email or password. Try again."
                     });
+                    this.props.handleUnSuccessfulAuth();
                 }
             }).catch(error => {
                 this.setState({
                     errorText: "An error occurred"
                 });
+                this.props.handleUnSuccessfulAuth();
             });
 
         event.preventDefault();

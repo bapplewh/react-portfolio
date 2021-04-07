337e9486d727c96f2c30021c772e7467f8efde6a
bapplewh <bapps.5@gmail.com>
2021-04-06 23:36:23 -0600

    finalizing functionality inside of modal and starting to clean up blog-detail page

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
+import ReactHtmlParser from "react-html-parser";
+
+import BlogFeaturedImage from "../blog/blog-featured-image";
 
 export default class BlogDetail extends Component {
     constructor(props) {
@@ -40,15 +43,11 @@ export default class BlogDetail extends Component {
         return (
             <div className="blog-container">
                 <div className="content-container">
-                    <div className="featured-image-wrapper">
-                        <img src={featured_image_url} />
-                    </div>
-
                     <h1>{title}</h1>
 
-                    <div className="content">
-                        {content}
-                    </div>
+                    <BlogFeaturedImage img={featured_image_url} />
+
+                    <div className="content">{ReactHtmlParser(content)}</div>
                 </div>
             </div>
         );
